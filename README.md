7fd46de68ee5af9d0121b1a32533f2e7411ce49e
Zeyad Elzanati <zaelzanati@avaya.com>
2023-09-07 12:35:47 +0400

    First Commit! 🚀

---
+++ README.md
@@ -0,0 +1,49 @@
+# AXP API Proxy (A&A and Admin)
+
+A backend component for Authorization with AXP, as well as proxying the Admin API requests, any APIs under the base path `/api/admin`
+
+## Installation (Native)
+
+The backend component is a simple `Node.js` server using `Express`, just receiving the request from the widget and proxying it to AXP, as well as holding the client secret to not expose it in the browser.
+
+To run the backend component `(axp-proxy-api)`
+
+- Update the `.env` file with your credentials and AXP base URL, for example:
+  - ```env
+    AXP_CLIENT_ID=YOUR_AXP_CLIENT_ID
+    AXP_CLIENT_SECRET=your-very-complex-axp-client-secret
+    AXP_BASE_URL=https://eu.cc.avayacloud.com
+    PORT=3001 #The port this Server will run on
+    SSL_MODE=on # value should be 'on' if HTTPS enabled or 'off' if diasbled
+    SSL_CERT_PATH=/etc/nginx/cert.pem
+    SSL_KEY_PATH=/etc/nginx/key.pem
+    ```
+- Run `npm install` to install the libraries and package dependencies.
+- Run `node app.js` to run the server.
+
+## Docker-ized Build & Deploy (Recommended)
+
+  You must have Docker & Docker Compose running in order to proceed with the below steps.
+
+#### Setup
+- Follow the same steps mentioned above for updating the `.env` file
+- Make sure if you have SSL on to have the certificates/keys in the `secrets` folder.
+  - (Optional) if you have them in another location on the host machine, you can update the docker-compose file and mount them on the paths configured in the `.env` file
+- If you updated the PORT in the `.env` file, update the `Dockerfile.dev` file and change the PORT to the one you chose.
+
+### Docker Build
+- Run the following command to build your Docker image.
+
+  $ docker build -f Dockerfile.dev -t axp-api-proxy:latest .
+
+### Docker Compose
+- Update the Ports in the `docker-compose-dev.yml` file to the port you configured in the `.env` file.
+- Run the following command to deploy your container.
+
+  $ docker-compose up -c docker-compose-dev.yml -d
+
+- To uninstall you can run (in the same folder)
+
+  $ docker-compose down
+
+
---
+++ app.js
@@ -0,0 +1,147 @@
+const express = require("express");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const compression = require("compression");
+const cors = require("cors");
+const axios = require("axios");
+const qs = require("qs");
+const https = require('https');
+const fs = require('fs');
+const app = express();
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+// parse and import .env file
+require("dotenv").config();
+
+// set security HTTP headers
+app.use(helmet());
+
+// parse json request body
+app.use(express.json());
+
+// parse urlencoded request body
+app.use(express.urlencoded({ extended: true }));
+
+// sanitize request data
+app.use(xss());
+
+// gzip compression
+app.use(compression());
+
+// enable cors
+app.use(cors());
+app.options("*", cors());
+
+/*
+  Request Logger
+*/
+
+app.use((req, res, next) => {
+  const { rawHeaders, httpVersion, method, socket, url } = req;
+  const { remoteAddress, remoteFamily } = socket;
+
+  console.log(
+    JSON.stringify({
+      method,
+      url,
+      timestamp: Date.now(),
+      rawHeaders,
+      httpVersion,
+      remoteAddress,
+      remoteFamily,
+    })
+  );
+  next();
+});
+
+/*
+  Authorization API
+*/
+app.post(
+  "/auth/realms/:account_id/protocol/openid-connect/token",
+  (req, res, next) => {
+    if (req.body.client_id != process.env.AXP_CLIENT_ID)
+      res.status(401).send({ error: "Bad credentials" });
+    const data = qs.stringify({
+      grant_type: req.body.grant_type,
+      client_id: req.body.client_id,
+      client_secret: process.env.AXP_CLIENT_SECRET,
+      refresh_token: req.body.refresh_token,
+      username: req.body.username,
+      password: req.body.password
+    });
+    const config = {
+      method: "post",
+      url: `${process.env.AXP_BASE_URL}/auth/realms/${req.params.account_id}/protocol/openid-connect/token`,
+      data: data,
+    };
+    return axios(config)
+      .then(function (response) {
+        return res.send(response.data);
+      })
+      .catch((error) => {
+        return res.send(error.response.data);
+      });
+  }
+);
+
+/*
+  Update Single User API
+*/
+
+app.put(
+  "/api/admin/user/v1beta/accounts/:account_id/users/:user_id",
+  (req, res, next) => {
+    const config = {
+      method: "put",
+      url: `${process.env.AXP_BASE_URL}/api/admin/user/v1beta/accounts/${req.params.account_id}/users/${req.params.user_id}`,
+      data: req.body,
+      headers: { Authorization: req.get("Authorization") },
+    };
+    return axios(config)
+      .then(function (response) {
+        return res.send(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+        return res.send(error.response.data);
+      });
+  }
+);
+
+/*
+  Proxy All Other Administartion APIs
+*/
+const apiProxy = createProxyMiddleware("/api/admin", {
+  target: process.env.AXP_BASE_URL,
+  changeOrigin: true,
+});
+
+/*
+  Route Configuration
+*/
+
+app.use("/api", apiProxy);
+
+/*
+  SSL Configuration & Server Creation
+*/
+
+if (process.env.SSL_MODE == "on") {
+  const options = {
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+  };
+
+  https.createServer(options, app).listen(process.env.PORT, () => {
+    console.log(
+      "[SSL MODE ON] Started AXP Auth/Admin API Proxy Server on port: " + process.env.PORT
+    );
+  });
+}
+else
+  app.listen(process.env.PORT, () => {
+    console.log(
+      "[SSL MODE OFF] Started AXP Auth/Admin API Proxy Server on port: " + process.env.PORT
+    );
+  });
